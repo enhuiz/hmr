4d9ad1379683b61c6ddb8517fd8cecca3d95a69a
enhuiz <niuzhe.nz@outlook.com>
2019-05-10 17:01:28 +0800

    add opts

--- scripts/data/remove_failures.py
+++ scripts/data/remove_failures.py
@@ -6,20 +6,24 @@ import pandas as pd
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
+    parser.add_argument('styles', nargs='+',
+                        choices=['printed', 'written'],
+                        help='style considered to filter.')
     args = parser.parse_args()
     return args
 
 
-def remove_failures(path):
+def remove_failures(opts):
+    path = opts.path
     df = pd.read_csv(path, names=['id', 'annotation'])
     prev_len = len(df)
     if prev_len == 0:
         return
 
-    typ = path.split('/')[-1].split('.')[0]
+    part = path.split('/')[-1].split('.')[0]
     features_dir = os.path.join(*path.split('/')[:-2], 'features')
-    for style in ['printed', 'written']:
-        image_path = os.path.join(features_dir, style, typ, '{}.png')
+    for style in opts.styles:
+        image_path = os.path.join(features_dir, style, part, '{}.png')
         df['image'] = df['id'].apply(image_path.format)
         df = df[df['image'].apply(os.path.exists)]
 
@@ -32,4 +36,4 @@ def remove_failures(path):
 
 if __name__ == "__main__":
     args = get_args()
-    remove_failures(args.path)
+    remove_failures(args)
